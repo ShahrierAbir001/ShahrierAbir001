87fefd489f802375c00be6644ecd51fb310e8280
ShahrierAbir001 <lamyeajalal267@gmail.com>
2025-09-28 18:50:14 +0600

    Add files via upload

---
+++ problem_01.py
@@ -0,0 +1,8 @@
+dictionary = {
+    "Buddhiman": "Intelligent",
+    "Dhoni": "rich",
+    "Nondoniyo": "Aesthetic",
+}
+
+word = input("Enter the word you want the meaning of: ")
+print(dictionary[word])
